23c6e0fb9de9a93b4a00ade35ceb2ffd978915e5
Carlos Alberto <carmantillab@gmail.com>
2017-12-21 00:56:14 -0500

    Organizando imports

--- src/main/java/com/cmancode/load/app/LoadDataInfileApplication.java
+++ src/main/java/com/cmancode/load/app/LoadDataInfileApplication.java
@@ -2,7 +2,6 @@ package com.cmancode.load.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootApplication
 public class LoadDataInfileApplication{
